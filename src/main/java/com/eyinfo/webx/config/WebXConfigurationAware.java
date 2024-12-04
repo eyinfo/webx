3839ce141123be28879dbd60d803a6c57a9898da
ethan <ljh0576123@163.com>
2024-12-04 19:02:17 +0800

    设置默认提示消息

--- src/main/java/com/eyinfo/webx/config/WebXConfigurationAware.java
+++ src/main/java/com/eyinfo/webx/config/WebXConfigurationAware.java
@@ -1,5 +1,7 @@
-package com.eyinfo.webx;
+package com.eyinfo.webx.config;
 
+import com.eyinfo.foundation.entity.Result;
+import com.eyinfo.webx.handle.DefaultMessageHandle;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -13,5 +15,7 @@ public class WebXConfigurationAware implements ApplicationContextAware {
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
+        DefaultMessageHandle messageHandle = new DefaultMessageHandle();
+        Result.setMessageConfig(messageHandle.getDefaultMessageHandle());
     }
 }
